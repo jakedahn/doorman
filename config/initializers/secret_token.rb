84d7d4f577db3783ab462bd1b3d8139b1e76e3c9
Jake Dahn <admin@jakedahn.com>
2011-01-25 22:15:02 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorman::Application.config.secret_token = 'd57756db00dfae21ffcf1dafa72c768dcb35cdf7ffc9f2b3425192d29b348b012693442403bdcba7026edbc732887645a5665b8b41878775025623186271bfed'
